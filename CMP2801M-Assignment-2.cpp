52baf5192d039a197c67235d9c6ba6f0ea8dc414
Logan King <65763483+loganator956@users.noreply.github.com>
2023-01-05 17:28:32 +0000

    Initial commit

---
+++ CMP2801M-Assignment-2.cpp
@@ -0,0 +1,9 @@
+// CMP2801M-Assignment-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Logan King
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
