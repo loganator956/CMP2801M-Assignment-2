1e73d60c5e631d18b3fdbe2a44aaabf688887f20
Logan King <65763483+loganator956@users.noreply.github.com>
2023-01-12 15:33:29 +0000

    Add comments to movable class

--- Movable.h
+++ Movable.h
@@ -1,10 +1,21 @@
 #pragma once
+/// <summary>
+/// Provides an interface to move Shape objects
+/// </summary>
 class Movable
 {
-private:
-
 public:
+	/// <summary>
+	/// Move (Translate) a shape position
+	/// </summary>
+	/// <param name="x">Amount to move shape on x axis</param>
+	/// <param name="y">Amount to move shape on y axis</param>
 	virtual void move(int x, int y) = 0;
+	/// <summary>
+	/// Scale a shape, anchored to leftTop
+	/// </summary>
+	/// <param name="x">Amount to scale shape on X axis (Or both if square or circle)</param>
+	/// <param name="y">Amount to scale shape on Y axis (ignored if square or circle)</param>
 	virtual void scale(int x, int y) = 0;
 };
 
