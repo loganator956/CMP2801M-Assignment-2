c9256d49fb18a7c69382c6009f5936c113a01c29
Logan King <65763483+loganator956@users.noreply.github.com>
2023-01-12 15:01:34 +0000

    Make points a vector of pointers

--- Shape.h
+++ Shape.h
@@ -7,7 +7,7 @@ class Shape
 private:
 	float area;
 	float perimeter;
-	std::vector<Point> points;
+	std::vector<Point*> points;
 public:
 	std::vector<Point*> get_points();
 	void set_points(std::vector<Point*>);
